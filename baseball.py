005b1fac84dc9be83820844223c1b67a50f96779
SOONSIM KIM <116074978+Soonsim94@users.noreply.github.com>
2022-10-31 17:52:26 +0900

    Add files via upload

--- baseball.py
+++ baseball.py
@@ -1,72 +1,59 @@
 '''
 filename : baseball.py
-숫자야구게임 
+숫자야구게임
 '''
 
 # 컴퓨터 난수 3개
 from random import randint
 import sys
 
-c1 = randint(1,9)
-c2 = randint(1,9)
-c3 = randint(1,9)
+while True:
+    c1 = randint(1,9)
+    while True:
+        c2 = randint(1,9)
+        if c1 != c2:
+            break
+    c3 = randint(1,9)
+    print(c1,c2,c3)
+    cnt = 0  #시도횟수
+    s = 0
+    while s < 3 and cnt < 3:   
+        cnt +=1
+        # 게이머가 숫자 3개 입력
+        n1,n2,n3 = input().split(' ')
 
-cnt = 0 #시도횟수
+        # 정수형 변환
+        n1 = int(n1)
+        n2 = int(n2)
+        n3 = int(n3)
 
-s = 0
-while s < 3 and cnt < 5:  
-    cnt +=1
-    # 게이머가 숫자 3개 입력
-    n1,n2,n3 = input().split(' ') 
+        # 스트라익, 볼 초기화
+        s = 0
+        b = 0
 
-    # 정수형 변환
-    n1 = int(n1)
-    n2 = int(n2)
-    n3 = int(n3)
+        # 스트라익 수 계산
+        if c1 == n1:
+            s += 1
+        if c2 == n2:
+            s += 1    
+        if c3 == n3:
+            s += 1
 
-    #스트라이크, 볼 초기화 
-    s = 0 
-    b = 0 
+        # 볼 수 계산
+        if c1 == n2 or c1 == n3:
+            b += 1
+        if c2 == n1 or c2 == n3:
+            b += 1
+        if c3 == n1 or c3 == n1:
+            b += 1          
 
-    # 스트라익 수 계산 
+        print(f'스트라익:{s} ball:{b}')
 
-    if c1 == n1:
-        s += 1
-
-    if c2 == n2:
-        s += 1 
-
-    if c3 == n3:
-        s += 1
-
-    # 볼 수 계산 
-    if c1 == n2 or c1 == n3:
-        b += 1 
-
-    if c2 == n1 or c2 == n3:
-        b += 1 
-
-    if c3 == n1 or c3 == n2:
-        b += 1 
-
-    print(f'스트라이크:{s} ball:{b}')
-
+    exitYn = input("게임을 계속할까요? Y,N")
+    if exitYn in ['N','n']:
+        break
 #스트라익 3이면 "축하"
-    #시도횟수 3번만에 "천재"
+    #시도횟수 3번이하면 "천재"
     #시도횟수 4번만에 "우수"
-    #아니면 "천재"
+    #아니면 "보통"
 #스트라익이 3이 아니면 "게임오버"
-
-if s == 3:
-    print("축하")
-    if cnt == 3:
-        print("천재")
-    elif cnt == 4:
-        print("우수")
-    else :
-        print("보통")
-else :
-    print("게임오버")              
-
-print("게임종료")
-    
--- break.py
+++ break.py
@@ -1,63 +1,37 @@
 '''
 filename : break.py
 '''
-
-
-users = [ { 'userno':100, 'username':'user1', 'salary':2000},   #객체값 
-          { 'userno':101, 'username':'user2', 'salary':1000}, 
-          { 'userno':102, 'username':'user3', 'salary':1500}
-]
-
+users = [ {'userno':100, 'username':'user1', 'salary':2000},
+          {'userno':101, 'username':'user2', 'salary':1000},
+          {'userno':102, 'username':'user3', 'salary':1500}] 
 while True:
-    print("1.추가 2. 삭제 3. 조회 4. 전체조회 5.종료")
+    print("1.추가 2.삭제 3.조회 4.전체조회 5.종료")
     no = int(input("메뉴선택:"))
     if no == 1:
-        print("추가")
-        info={}   #변수선언 객체받을거다 
-        info["userno"] =int(input("userno입력:")) 
-        info["username"] =input("username입력:")
-        info["salary"] =input("salary입력:")  #번호 int 사용!
+        info = { }
+        info["userno"] = int(input("userno입력:"))
+        info["username"] = input("username입력:")
+        info["salary"] = int(input("salary입력:")) 
         users.append(info)
-
-
-#문자열과 리스트인 경우만 in을 씀. 
     elif no == 2:
-        userno = int(input("삭제할 번호 입력:"))
+        userno = input("삭제할 번호 입력:")
         idx = 0;
         for obj in users:
             if obj["userno"] == userno:
                 del users[idx]
-                break   
+                break 
             idx += 1
-
-
     elif no == 3:
-        #번호를 입력(검색할번호입력)
+        #번호를 입력
         userno = int(input("조회할 번호 입력:"))
-        for idx in users:
+        #해당 번호의 이름과 급여 출력
+        for obj in users:
             if obj['userno'] == userno :
-                print(f'{users[obj]["username"]} {users[obj]["salary"]}')
-        #해당 번호의 이름과 급여 출력 
-
+                print(f'{obj["userno"]} \t {obj["salary"]}' )
     elif no == 4:
-        print("전체조회")
-        for obj in users :
-            print(f'{obj['userno']} \t {obj['salary']}')
-
-        #for문(전체출력)
+        print("전체조회")    
+        for obj in users:
+            print(f'{obj["userno"]} \t {obj["salary"]}' )               
     elif no == 5:
         break
 print("the end")
-
-
-
-# name =input("검색할이름입력:")
-# for i in range(len(members)):  
-#     if name in members[i]["name"]:     
-
-#         print(f'{members[i]["name"]} \t {members[i]["addr"]}')
-   
-#         del members[i]    
-    
-#         break
-#     print(members)
